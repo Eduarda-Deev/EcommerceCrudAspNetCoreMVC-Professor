f487ba72dfd0864250c73128ae879f77f8a2f0cb
EDUARDA DA SILVA GORRI <eduarda.gorri@portalsesisp.org.br>
2026-03-26 14:56:50 -0300

    Atualizou perfil, imagem, etc

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,16 @@ namespace Esse_deu_certo_MVC_Exemplo.Controllers
             return View();
         }
 
+        public IActionResult Sobre()
+        {
+            return View();
+        }
+
+        public IActionResult Feedback()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
